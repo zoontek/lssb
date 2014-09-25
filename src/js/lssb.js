62ab215f97fdb9f7d7d6f338f3e423abdcc3f564
Acthernoene Mathieu <zoontek@gmail.com>
2014-09-26 01:26:32 +0200

    full data attributes support

--- src/js/lssb.js
+++ src/js/lssb.js
@@ -1,46 +1,77 @@
 !function() {
+    // common pointers
+    var w = window,
+        d = document,
+        s = screen;
+
+    function click(e) {
+        e.preventDefault();
+        var t = this;
+
+        var classes = t.className.split(' ');
+        var href = t.getAttribute('href');
+
+        var url = getData(t, 'url');
+        if (null === url) url = w.location.href; // fallback to window href
+
+        if (-1 !== classes.indexOf('lssb-fb')) { // facebook
+            href += '?u=' + encode(url);
+        }
+        else {
+            href += '?url=' + encode(url);
+
+            if (-1 !== classes.indexOf('lssb-tw')) { // twitter
+                var hashtags = getData(t, 'hashtags');
+                var related = getData(t, 'related');
+                var via = getData(t, 'via');
+                var text = getData(t, 'text');
+                if (null === text) text = d.title; // fallback to window title
+
+                href += '&text=' + encode(text);
+                if (null !== hashtags) href += '&hashtags=' + encode(hashtags);
+                if (null !== related) href += '&related=' + encode(related);
+                if (null !== via) href += '&via=' + encode(via);
+            }
+        }
+
+        var height = getData(t, 'height');
+        if (null === height) height = 410; // fallback if not height
+        var width = getData(t, 'width');
+        if (null === width) width = 580; // fallback if not width
+
+        popup(
+            href,
+            t.querySelector('.lssb-text').textContent,
+            height,
+            width
+        );
+    }
+
     function popup(url, title, height, width) {
-        var st = window.screenTop;
-        var sl = window.screenLeft;
-        var ih = window.innerHeight;
-        var iw = window.innerWidth;
-        var ch = document.documentElement.clientHeight;
-        var cw = document.documentElement.clientWidth;
-
-        var screenTop = void(0) !== st ? st : screen.top;
-        var screenLeft = void(0) !== sl ? sl : screen.left;
-        var windowHeight = ih ? ih : ch ? ch : screen.height;
-        var windowWidth = iw ? iw : cw ? cw : screen.width;
-
-        var top = windowHeight / 2 - height / 2 + screenTop;
-        var left = windowWidth / 2 - width / 2 + screenLeft;
-
-        var popup = window.open(
+        var popup = w.open(
             url,
             title,
             'scrollbars=yes,width=' + width
             + ',height=' + height
-            + ',top=' + top
-            + ',left=' + left
+            + ',top=' + (w.innerHeight / 2 - height / 2 + w.screenY)
+            + ',left=' + (w.innerWidth / 2 - width / 2 + w.screenX)
         );
 
-        if (window.focus) {
+        if (w.focus) {
             popup.focus();
         }
     }
 
-    function click(event) {
-        event.preventDefault();
-        popup(
-            this.getAttribute('href'),
-            this.querySelector('.lssb-text').textContent,
-            this.getAttribute('data-height'),
-            this.getAttribute('data-width')
-        );
+    function encode(component) { // short for encodeURIComponent
+        return encodeURIComponent(component);
     }
 
-    document.addEventListener('DOMContentLoaded', function() {
-        var links = document.querySelectorAll('.lssb-btn.popup');
+    function getData(element, attribute) { // get data-* attribute
+        return element.getAttribute('data-' + attribute);
+    }
+
+    d.addEventListener('DOMContentLoaded', function() {
+        var links = d.querySelectorAll('.lssb-btn');
         var i = 0;
 
         for (; i < links.length; i++) {
